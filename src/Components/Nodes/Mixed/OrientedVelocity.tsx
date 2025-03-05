9d94d41e23053cf89c268b89ed65391b872a4a81
Conikku <72884174+Conikku@users.noreply.github.com>
2025-03-05 11:50:39 -0500

    Update OrientedVelocity.tsx

--- src/Components/Nodes/Mixed/OrientedVelocity.tsx
+++ src/Components/Nodes/Mixed/OrientedVelocity.tsx
@@ -1,15 +1,30 @@
+// src/API/Nodes/Mixed/OrientedVelocity.ts
+
+import { ConnectableNumberField } from "API/Fields/ConnectableNumberField";
+import { ConnectableVector3Field } from "API/Fields/ConnectableVector3Field";
+import type { ParticleData } from "API/ParticleService";
+import { MixedNode } from "./MixedNode";
+
 export class OrientedVelocity extends MixedNode {
     static className = "OrientedVelocity";
 
     nodeFields = {
-        parentReference: new ConnectableVector3Field(0, 0, 0),
+        direction: new ConnectableVector3Field(0, 0, 0),
         speed: new ConnectableNumberField(1),
         upVector: new ConnectableVector3Field(0, 1, 0),
     };
 
     Run(data: ParticleData) {
-        const lookVector = this.nodeFields.parentReference.GetVector3(data);
+        const direction = this.nodeFields.direction.GetVector3(data);
         const speed = this.nodeFields.speed.GetNumber(data);
-        data.velocityNormal = lookVector.mul(speed);
+        
+        if (direction.Magnitude > 0) {
+            const velocity = direction.mul(speed);
+            data.velocityNormal = new Vector3(velocity.X, velocity.Y, velocity.Z);
+        }
+    }
+
+    GetClassName(): string {
+        return OrientedVelocity.className;
     }
 }
